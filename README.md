45c0375f0eedce9bf130714c39f5a07049860968
Greg Bergé <neoziro@gmail.com>
2014-11-18 21:51:50 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,15 @@
+# fb-challenge
+
+Facebook Challenge.
+
+## Run
+
+```
+npm install
+bower install
+gulp
+```
+
+## License
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+// Static server.
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch('style.css', function () {
+    browserSync.reload(['style.css']);
+  });
+
+  gulp.watch('index.html', function () {
+    browserSync.reload(['index.html']);
+  });
+});
