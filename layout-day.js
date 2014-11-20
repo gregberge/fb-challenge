f69502378f1815ab1a28eb8cda518a7a3cdd4fb0
Greg Bergé <neoziro@gmail.com>
2014-11-20 10:49:06 +0100

    Stable with generator.

--- layout-day.js
+++ layout-day.js
@@ -1,129 +1,118 @@
 (function () {
-      window.layOutDay = layOutDay;
+  // Expose method on global scope.
+  window.layOutDay = layOutDay;
 
-      // DOM Elements representing events.
-      var elements = [];
+  // Event template.
+  var eventTemplate = $('#event-template').html();
 
-      // Event template.
-      var eventTemplate = $('#event-template').html();
+  // Calendar.
+  var calendar = $('.calendar');
 
-      // Calendar.
-      var calendar = $('.calendar');
+  // Maximum width to display events.
+  var maxWidth = 600;
 
-      // Maximum width to display events.
-      var maxWidth = 600;
+  /**
+   * Lay out the events on the calendar.
+   *
+   * @param {object[]} events
+   */
 
-      /**
-       * Lay out the events on the calendar.
-       *
-       * @param {object[]} events
-       */
+  function layOutDay(events) {
+    // Clean the calendar.
+    clean();
 
-      function layOutDay(events) {
-        // Clean the calendar.
-        clean();
+    // Do nothing if it's not a valid array.
+    if (!Array.isArray(events)) return;
 
-        // Do nothing if it's not a valid array.
-        if (!Array.isArray(events)) return;
+    // Sort events by range to optimize positioning.
+    events.sort(function byRange(a, b) {
+      return (b.end - b.start) - (a.end - a.start);
+    });
 
-        // Create a map that will contains the events.
-        var map = new Array(721);
+    // Compute dimension and position of events.
+    events.forEach(function computeDimensionAndPosition(event) {
+      // Add dimension and neighbours.
+      event.dimension = 0;
+      event.neighbours = getNeighbourgs(event);
 
-        // Prepare events and map.
-        events.forEach(function prepareEvents(event) {
-          // Prepare map.
-          for(var i = event.start; i <= event.end; i++) {
-            map[i] = map[i] || [];
-            map[i].push(event);
-          }
+      // Compute position of the event.
+      event.position = event.neighbours.reduce(function (position, neigbour) {
+        if (neigbour.position === position) position++;
+        return position;
+      }, 0);
 
-          // Add dimension and neighbours.
-          event.dimension = getMaxNeighbours(map, event);
-          event.neighbours = getNeighbourgs(map, event);
-        });
+      // Adjust dimension according to position.
+      if (event.position >= event.dimension)
+        event.dimension = event.position + 1;
+    });
 
-        // Sort events by range to optimize positionning.
-        events.sort(function byRange(a, b) {
-          return (b.end - b.start) - (a.end - a.start);
-        });
+    // Sort by dimension to optimize dimension propagation.
+    events.sort(function byDimension(a, b) {
+      return b.dimension - a.dimension;
+    });
 
-        events.forEach(function (event, idx) {
-          // Sort neighbours.
-          event.neighbours.sort(function (a, b) {
-            return (a.position || 0) - (b.position || 0);
-          });
-
-          event.position = event.neighbours.reduce(function (position, neigbour) {
-            if (neigbour.position === position) position++;
-            return position;
-          }, 0);
-
-          event.index = idx;
-
-          if (event.position >= event.dimension) {
-            event.dimension = event.position + 1;
-          }
-        });
-
-        // Sort event by dimensions.
-        events.sort(function byDimension(a, b) {
-          return b.dimension - a.dimension;
-        });
-
-        events.forEach(synchronizeDimension);
-
-        function synchronizeDimension(event) {
-          event.neighbours.forEach(function (neigbour) {
-            if (neigbour.dimension < event.dimension) {
-              neigbour.dimension = event.dimension;
-            }
-          });
+    // Propagate event dimension to neighbourgs.
+    events.forEach(function propagateDimension(event) {
+      event.neighbours.forEach(function (neigbour) {
+        if (neigbour.dimension < event.dimension) {
+          neigbour.dimension = event.dimension;
         }
+      });
+    });
 
-        // Add each event to the calendar.
-        events.forEach(createEvent);
+    // Add element to the calendar.
+    events.forEach(function addElement(event) {
+      // Compute size and positionning.
+      var width = maxWidth * (1 / event.dimension) - 22;
+      var height = event.end - event.start - 10;
+      var top = event.start;
+      var left = maxWidth * (event.position / event.dimension);
 
-        function createEvent(event) {
-          if (event.index === 45) console.log(event);
-          var w = maxWidth * (1 / event.dimension) - 22;
-          var l = maxWidth * (event.position / event.dimension);
+      // Create element.
+      var element = $(eventTemplate);
+      element.css('top', top);
+      element.css('height', height);
+      element.css('width', width);
+      element.css('margin-left', left);
 
-          var element = $(eventTemplate);
-          element.find('.title').html(event.index);
-          element.css('top', event.start + 'px');
-          element.css('height', (event.end - event.start - 10) + 'px');
-          element.css('width', w);
-          element.css('margin-left', l);
-          calendar.append(element);
-          elements.push(element);
-        }
+      // Add element on the calendar.
+      calendar.append(element);
+    });
 
-        function getNeighbourgs(map, event) {
-          return events.filter(function (ev) {
-            return intersects(ev, event);
-          });
-        }
 
-        function getMaxNeighbours(map, event) {
-          return Math.max.apply(null, map.slice(event.start, event.end).map(function (ar) {
-            return ar ? ar.length : 0;
-          }));
-        }
+    /**
+     * Return neighbours of an event.
+     * Neigbourgs are events that interesects.
+     *
+     * @param {object} event
+     */
 
-        function intersects(a, b) {
-          return !((a.end < b.start) || (b.end < a.start));
-        }
+    function getNeighbourgs(event) {
+      return events.filter(function (ev) {
+        return intersects(ev, event);
+      })
+      .sort(function (a, b) {
+        return (a.position || 0) - (b.position || 0);
+      });
+    }
 
-        /**
-         * Clean the calendar.
-         * Remove all elements.
-         */
+    /**
+     * Test if two events intersects.
+     *
+     * @param {object} eventA
+     * @param {object} eventB
+     */
 
-        function clean() {
-          elements.forEach(function (element) {
-            element.remove();
-          });
-          elements = [];
-        }
-      }
-    }());
+    function intersects(eventA, eventB) {
+      return !((eventA.end < eventB.start) || (eventB.end < eventA.start));
+    }
+
+    /**
+     * Clean the calendar by removin all elements.
+     */
+
+    function clean() {
+      calendar.empty();
+    }
+  }
+}());
